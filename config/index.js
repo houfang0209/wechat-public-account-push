4efab9e3538b8f8e39b4b261f46df469be3ab518
houfang0209 <112925809+houfang0209@users.noreply.github.com>
2022-09-06 20:55:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,28 +4,12 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  // APP_ID: "wx7ce941ef70ac956e",
+   APP_ID: "wx7ce941ef70ac956e",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  // APP_SECRET: "8f1fedacbaaa66816a56535bdd7b880e",
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "高妍",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "oMN5-6UgKdIZ0a2YVOxCFMFaMOjA",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "_5NOAnO6g_5FLhweUsHB8AM7Ecm2yEm2qphZo0iAGgY",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
-   */
+   APP_SECRET: "8f1fedacbaaa66816a56535bdd7b880e",
   USERS: [
     {
       // 想要发送的人的名字
